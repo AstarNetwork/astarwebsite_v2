1cd9ce8a2f0b38194edc3123032c2413dfa38ba9
Ayumi Takahashi <ayumee528@gmail.com>
2023-09-12 00:08:44 -0700

    Add a new hero image

--- components/techstack/Hero.vue
+++ components/techstack/Hero.vue
@@ -2,16 +2,28 @@
   <div
     class="landscape:min-h-[500px] landscape:lg:min-h-[800px] portrait:min-h-[80vh] flex items-center justify-start"
   >
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 relative z-10 w-full">
-      <div class="lg:w-2/3 py-12">
+    <div
+      class="max-w-7xl mx-auto px-4 sm:px-6 relative z-10 w-full sm:flex sm:items-center"
+    >
+      <div class="sm:w-3/5 py-12">
         <h1
-          class="text-4xl sm:text-5xl lg:text-6xl font-extrabold tracking-tight drop-shadow pt-24 sm:pt-32 lg:pt-0 leading-tight"
+          class="text-4xl sm:text-5xl lg:text-6xl font-extrabold tracking-tight drop-shadow leading-tight"
         >
           {{ $t("techstack.title") }}
         </h1>
         <p class="sm:text-lg mt-4">
           {{ $t("techstack.description") }}
         </p>
+      </div>
+      <div class="sm:w-2/5">
+        <img
+          class="w-full max-w-[300px] sm:max-w-none mx-auto"
+          src="/images/techstack/hero.svg"
+          :alt="$t('techstack.title')"
+          width="476"
+          height="334"
+          data-not-lazy
+        />
       </div>
     </div>
   </div>
