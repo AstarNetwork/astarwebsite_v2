d46b1b8acda92a96159ef39a58667326835b1e70
Ayumi Takahashi <ayumee528@gmail.com>
2023-09-13 11:49:51 -0700

    update japanese faqs

--- components/techstack/FAQs.vue
+++ components/techstack/FAQs.vue
@@ -3,9 +3,6 @@
     <h2 class="title text-center mb-12">
       <span>FAQs</span>
     </h2>
-    <p v-if="locale === 'ja'" class="mb-4 sm:mb-6">
-      ※近日中に日本語版FAQにアップデートいたします
-    </p>
     <dl
       class="space-y-6 sm:space-y-10 divide-y divide-white/20 border-t border-b border-white/20 pb-6 sm:pb-10"
     >
@@ -20,8 +17,7 @@
             class="flex w-full items-start justify-between text-left text-white"
           >
             <span class="text-base sm:text-xl font-medium">
-              <!-- {{ locale === "ja" ? faq.question.ja : faq.question.en }} -->
-              {{ faq.question.en }}
+              {{ locale === "ja" ? faq.question.ja : faq.question.en }}
             </span>
             <span class="ml-6 flex h-7 items-center">
               <PlusSmallIcon v-if="!open" class="h-6 w-6" aria-hidden="true" />
@@ -30,8 +26,7 @@
           </DisclosureButton>
         </dt>
         <DisclosurePanel as="dd" class="mt-6 pr-12 techstack-faqs__answer">
-          <!-- <div v-html="locale === 'ja' ? faq.answer.ja : faq.answer.en" /> -->
-          <div v-html="faq.answer.en" />
+          <div v-html="locale === 'ja' ? faq.answer.ja : faq.answer.en" />
         </DisclosurePanel>
       </Disclosure>
     </dl>
@@ -69,14 +64,13 @@ const faqs = [
         <li>Trustless interoperability with Ethereum and making diverse integrations smoother</li>
         <li>Significantly reduced transaction costs compared to Ethereum, making it economically advantageous for users</li>
       </ul>`,
-      ja: `<p>Astar zkEVMは、Web3の社会実装を実現するグローバル企業向けの汎用的なソリューションです。特に世界的なゲームやエンターテインメント事業が日本市場に参入するためのゲートウェイとして位置づけられています。Ethereumレイヤー2ソリューションの実装はAstarにとって革新的なアップデートであります。</p>
-      <p>Astar zkEVMの主な特徴は以下の通りです：</p>
+      ja: `<p>Astar zkEVMは、Web3の社会実装を実現するグローバル企業向けの汎用的なソリューションです。特にゲームやエンターテインメントを含む、グローバルに展開している企業がWeb3市場に参入するためのゲートウェイとして位置づけられています。Ethereumレイヤー2ソリューションの実装はAstarにとって革新的なアップデートです。Astar zkEVMの主な特徴は以下の通りです：</p>
       <ul>
-        <li>ZKテクノロジーにより、大量のトランザクションが発生してもスムーズな運用を保証する拡張性</li>
+        <li>ZKテクノロジーにより、大量のトランザクションが発生してもスムーズな運用を保証する拡張性（スケーラビリティ）</li>
         <li>EVM等価性を備え、EthereumやPolygon等の既存のコントラクトをコードの変更なしに使用可能</li>
-        <li>Ethereumからセキュリティメカニズムを導入し、堅牢で安全なフレームワークを実現</li>
-        <li>Ethereumとの信頼性の高い相互運用性により、様々な統合をよりスムーズに実現可能</li>
-        <li>Ethereumと比較して取引コストが大幅に削減され、ユーザーにとって経済的なUXを実現</li>
+        <li>Ethereumからセキュリティメカニズムを継承し、堅牢で安全なフレームワークを実現</li>
+        <li>Ethereumとの高い相互運用性により、様々な統合をよりスムーズに実現可能</li>
+        <li>Ethereumと比較してガス代が大幅に削減され、ユーザーにとって経済的なUXを実現</li>
       </ul>`,
     },
   },
@@ -95,11 +89,11 @@ const faqs = [
       <li>Trustless interoperability with Ethereum</li>
       <li>Significantly reduced transaction cost compared to Ethereum mainnet</li>
     </ul>`,
-      ja: `<p>Astar zkEVMにはいくつかのユニークな点があります：</p>
+      ja: `<p>Astar zkEVMには複数のユニークな点があります：</p>
       <ul>
-        <li>グローバルプロジェクト、企業、開発者と日本市場との架け橋</li>
+        <li>日本市場におけるグローバルプロジェクト、企業、開発者の架け橋</li>
         <li>Ethereumから継承した高いセキュリティ</li>
-        <li>高度なEVM等価性</li>
+        <li>高いEVM等価性</li>
         <li>ZKテクノロジーによる高い拡張性</li>
         <li>Ethereumとのトラストレスな相互運用性</li>
         <li>Ethereumメインネットと比較して格段に安いガス代</li>
@@ -109,7 +103,7 @@ const faqs = [
   {
     question: {
       en: "What is Astar zkEVM’s gas option?",
-      ja: "Astar zkEVMのガス代としてのトークンの選択肢は？",
+      ja: "Astar zkEVMのガス代にはどのトークンが使用されますか？",
     },
     answer: {
       en: "We will use ETH as the gas token on Astar zkEVM testnet.",
@@ -119,12 +113,12 @@ const faqs = [
   {
     question: {
       en: "What is the difference between Astar zkEVM and Ethereum mainnet?",
-      ja: "Astar zkEVMとEhereumメインネットの違いは？",
+      ja: "Astar zkEVMとEthereumメインネットの違いは？",
     },
     answer: {
       en: `Astar zkEVM distinguishes itself from the Ethereum mainnet primarily by offering a much lower transaction cost (gas fees) and a greater transaction throughput. <br />
       This enhanced throughput, combined with reduced transaction costs, provides developers with the ideal environment to create applications demanding high transaction speeds without the burden of high costs.`,
-      ja: `Astar zkEVMは、安いガス代（トランザクションコスト）やより高い処理能力を提供することで、Ethereumのメインネットとの差別化を図っています。これにより、開発者はアプリをこれまで以上に理想的な環境で製作することができます。`,
+      ja: `Astar zkEVMは、安いガス代（トランザクションコスト）やより高い処理能力を提供することで、Ethereumのメインネットとの差別化を図っています。これにより、開発者はアプリケーションをこれまで以上に理想的な環境で構築することができます。`,
     },
   },
   {
@@ -143,28 +137,34 @@ const faqs = [
       </ul>`,
       ja: `<p>Ethereum上でレイヤー2を展開することは、Astarエコシステムにいくつかの利点をもたらします：</p>
       <ul>
-        <li><strong>豊富な開発環境</strong>：Astar Substrate（レイヤー1）とAstar zkEVM（レイヤー2）があるため、開発者はより多くの環境とツールを使って制作/構築することができます。</li>
-        <li>より大きなコミュニティへの拡大</li>
-        <li>Astarは、EthereumのTier 1プロジェクトとより密接に協力により、AstarのdAppステーキングから利益を得ることができるようになるなどAstar独自の機能を使用可能です。</li>
-        <li>ネットワークの成功にとって重要な要因である、取引の増加、アクティブユーザーの増加、流動性の増加などの恩恵を得ることができます。</li>
-        <li>より多くの日本企業などのユースケースがAstarエコシステムに参加する予定です。</li>
+        <li><strong>豊富な開発環境</strong>：Astar Substrate（レイヤー1）とAstar zkEVM（レイヤー2）があるため、開発者はより多くの環境とツールを使って開発・構築することができます。</li>
+        <li>Astarがより大きなコミュニティへ拡大します。</li>
+        <li>Ethereumのトッププロジェクトとの協力を深めることができます。これらのプロジェクトは、dAppステーキングをはじめとするAstarのさまざまな独自機能の恩恵を受けることができるようになり、また既存のAstarコミュニティはEthereumコミュニティとの信仰を深めることができます。</li>
+        <li>ネットワークの成功にとって重要な要因であるトランザクション・アクティブユーザー・流動性の増加などの恩恵を受けることができます。</li>
+        <li>より多くの日本企業などのユースケースがAstarエコシステムに参加します。</li>
       </ul>`,
     },
   },
   {
     question: {
       en: "What is the value of Astar zkEVM for ASTR token holders?",
-      ja: "",
+      ja: "ASTRトークン所有者にとって、Astar zkEVMの価値は何ですか?",
     },
     answer: {
       en: `<p>Astar zkEVM uses the ASTR token in three new ways: as the gas fee for tooling, in the burning mechanism with the sequencer, and burning incentivized tokens to the network aggregator. This model will be evaluated on our testnet to maximize the value for ASTR holders. The final design is subject to change based on analysis from testing the model.</p>
       <img
       class="w-full"
-              src="/images/techstack/astar-value.webp"
-              alt="Astar zkEVM Value"
-              data-not-lazy
-            />`,
-      ja: "",
+      src="/images/techstack/astar-value.webp"
+      alt="Astar zkEVM Value"
+      data-not-lazy
+      />`,
+      ja: `<p>Astar zkEVMは、ASTRトークンを3つの新しい方法で使用します。ツールのガス料金として、シーケンサーによる書き込みメカニズムで、およびネットワークアグリゲーターへのインセンティブ付きトークンの書き込みです。このモデルは、ASTR保有者の価値を最大化するためにテストネットで評価されます。最終的な設計は、モデルのテストからの分析に基づいて変更される可能性があります。</p>
+      <img
+      class="w-full"
+      src="/images/techstack/astar-value.webp"
+      alt="Astar zkEVM Value"
+      data-not-lazy
+      />`,
     },
   },
   {
@@ -174,7 +174,7 @@ const faqs = [
     },
     answer: {
       en: "Though transactions on Astar zkEVM will be significantly cheaper than those on Ethereum mainnet, it is nonetheless going be more expensive compared to Astar Substrate on Polkadot. The reason is that as Astar zkEVM is a Layer 2 solution of Ethereum, the cost of the network is largely affected by the cost of Ethereum mainnet, which is significantly higher than Astar Substrate.",
-      ja: "Astar zkEVMはEthereumメインネットに比べて格段に安くになる一方で、Polkadot上のAstar Substrateに比べると高くになります。理由としては、Astar zkEVMはレイヤー2ソリューションであるため、Astar Subtrateより高く、また、Ethereumメインネットの影響を大きく受けるためです。",
+      ja: "Astar zkEVMはEthereumメインネットに比べて格段に安くになる一方で、Polkadot上のAstar Substrateに比べると高くなる可能性があります。Astar zkEVMはEthereumのレイヤー2ソリューションであるため、Ethereum メインネットのガス代に大きく影響されます。",
     },
   },
 ];
