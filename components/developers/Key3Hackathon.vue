1039643aca44d8af8ac5aafb0583ee5106af8f98
Ayumi Takahashi <ayumee528@gmail.com>
2023-10-02 18:59:31 -0700

    Replace astar university to key hackathon

---
+++ components/developers/Key3Hackathon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="space-gradient rounded-3xl lg:w-1/2 min-h-[500px] sm:min-h-[700px] border border-[#E4CD5B] relative"
+    style="
+      background: url(/images/developers/key3-hackathon.webp) top center / cover
+        no-repeat;
+    "
+  >
+    <h3 class="sr-only">web3 Global Hackathon 2023 AW</h3>
+    <div
+      class="flex justify-center absolute bottom-[115px] sm:bottom-[170px] w-full"
+    >
+      <NuxtLink
+        class="block bg-[#BE0B24] text-white uppercase border-2 border-[#E4CD5B] text-center font-bold p-3 lg:p-4 w-60 rounded-lg hover:opacity-80"
+        to="https://key3.eventos.tokyo/web/portal/744/event/7845"
+        target="_blank"
+      >
+        Entry
+      </NuxtLink>
+    </div>
+  </div>
+</template>
--- components/developers/Learn.vue
+++ components/developers/Learn.vue
@@ -6,38 +6,13 @@
     <div
       class="max-w-7xl mx-auto lg:flex items-center lg:space-x-12 space-y-16 lg:space-y-0"
     >
-      <div class="space-gradient rounded-3xl px-6 pt-1 pb-16 lg:w-1/2">
-        <img
-          src="/images/developers/university.svg"
-          class="mx-auto max-w-[440px] mb-6 sm:mb-6 -mt-6 sm:-mt-16 w-full"
-        />
-        <h3 class="leading-tight mb-4 text-center">
-          <span class="text-3xl sm:text-4xl lg:text-5xl font-extrabold block">
-            Astar University
-          </span>
-          <small class="block text-lg sm:text-2xl">
-            Online training to gain Web3 programming skills
-          </small>
-        </h3>
-        <p class="mx-auto max-w-2xl mb-8 sm:text-lg lg:text-center">
-          {{ $t("developers.learn.university") }}
-        </p>
-        <div class="text-center">
-          <Button
-            variant="outlined"
-            size="lg"
-            href="/developers/university"
-            class="w-72"
-          >
-            Astar University ->
-          </Button>
-        </div>
-      </div>
+      <!-- <DevelopersAstarUniversity /> -->
+      <DevelopersKey3Hackathon />
       <div class="lg:w-1/2">
         <h3
           class="font-extrabold text-3xl lg:text-4xl mb-6 text-center sm:text-left"
         >
-          {{ $t("developers.learn.title") }}
+          {{ $t("developers.learn.docs.title") }}
         </h3>
         <ul class="space-y-6">
           <li v-for="item in docs">
