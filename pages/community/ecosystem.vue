6c9e09658c32a01276ed6d67a09f16dad1b7638e
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2024-04-12 17:41:47 +0900

    fix issues from SSG

--- pages/community/ecosystem.vue
+++ pages/community/ecosystem.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
 const localePath = useLocalePath()
 
-definePageMeta({
-  middleware: [
-    function ({ query }) {
-      return navigateTo({
-        path: localePath('/ecosystem'),
-        query,
-      }, { redirectCode: 301 })
-    },
-  ],
-})
+navigateTo({
+  path: localePath('/ecosystem'),
+}, { redirectCode: 301 })
 </script>
