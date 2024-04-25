52f868dbf07ef18e3cc355103a1a78c22e8f40f4
Ayumi Takahashi <ayumee528@gmail.com>
2024-04-25 09:11:44 +0900

    update

--- components/home/Developer.vue
+++ components/home/Developer.vue
@@ -25,7 +25,7 @@
             index === 0 ? 'pink' : 'blue'
           }.svg`"
           class="absolute left-0 top-0 w-full object-cover opacity-20"
-        />
+        >
         <div class="relative z-10">
           <p
             class="mb-6 flex items-center justify-end space-x-2 text-xs uppercase text-slate-300 sm:text-sm"
@@ -38,7 +38,7 @@
               class="h-5 sm:h-6"
               :src="item.poweredBy.image"
               :alt="item.poweredBy.text"
-            />
+            >
           </p>
           <h3 class="mb-4 text-2xl leading-snug text-white sm:mb-6 sm:text-3xl">
             {{ item.name }}
@@ -47,7 +47,11 @@
             {{ item.description }}
           </p>
           <ul class="my-10 space-y-2">
-            <li v-for="feature in item.features" :key="feature" class="flex">
+            <li
+              v-for="feature in item.features"
+              :key="feature"
+              class="flex"
+            >
               <CheckCircleIcon
                 :class="index === 0 ? 'text-pink' : 'text-blue'"
                 class="mr-2 h-5 w-5 shrink-0 sm:h-6 sm:w-6"
@@ -62,11 +66,17 @@
     <div
       class="mt-8 flex flex-col items-center justify-center gap-4 text-center sm:mt-12 sm:flex-row"
     >
-      <Button :href="localePath('/developers/')" color="whiteGray">
+      <Button
+        :href="localePath('/developers/')"
+        color="whiteGray"
+      >
         {{ $t("home.developer.cta.startBuilding") }}
       </Button>
 
-      <Button :href="docsUrl.networks" color="whiteGray">
+      <Button
+        :href="docsUrl.networks"
+        color="whiteGray"
+      >
         {{ $t("home.developer.cta.ourNetworks") }}
       </Button>
     </div>
@@ -74,45 +84,45 @@
 </template>
 
 <script setup lang="ts">
-const localePath = useLocalePath();
-import { CheckCircleIcon } from "@heroicons/vue/24/solid";
-import { docsUrl } from "@/data/links";
+const localePath = useLocalePath()
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { docsUrl } from '@/data/links'
 
-const { t } = useI18n();
+const { t } = useI18n()
 
 const data = [
   {
-    slug: "substrate",
-    name: "Astar Substrate",
-    description: t("home.developer.substrate.description"),
+    slug: 'substrate',
+    name: 'Astar Substrate',
+    description: t('home.developer.substrate.description'),
     poweredBy: {
-      image: "/images/home/polkadot-logo.svg",
-      text: "Polkadot",
+      image: '/images/home/polkadot-logo.svg',
+      text: 'Polkadot',
     },
     features: [
-      t("home.developer.substrate.features.multiVmSupport"),
-      t("home.developer.substrate.features.rustAndInk"),
-      t("home.developer.substrate.features.interoperabilityAndComposability"),
-      t("home.developer.substrate.features.secured"),
-      t("home.developer.substrate.features.dappStakingRewards"),
-      t("home.developer.substrate.features.lowFees"),
+      t('home.developer.substrate.features.multiVmSupport'),
+      t('home.developer.substrate.features.rustAndInk'),
+      t('home.developer.substrate.features.interoperabilityAndComposability'),
+      t('home.developer.substrate.features.secured'),
+      t('home.developer.substrate.features.dappStakingRewards'),
+      t('home.developer.substrate.features.lowFees'),
     ],
   },
   {
-    slug: "zkevm",
-    name: "Astar zkEVM",
-    description: t("home.developer.zkevm.description"),
+    slug: 'zkevm',
+    name: 'Astar zkEVM',
+    description: t('home.developer.zkevm.description'),
     poweredBy: {
-      image: "/images/home/polygon-logo.svg",
-      text: "Polygon",
+      image: '/images/home/polygon-logo.svg',
+      text: 'Polygon',
     },
     features: [
-      t("home.developer.zkevm.features.ethereumSecurity"),
-      t("home.developer.zkevm.features.highScalability"),
-      t("home.developer.zkevm.features.lowFees"),
-      t("home.developer.zkevm.features.access"),
-      t("home.developer.zkevm.features.developerExperience"),
+      t('home.developer.zkevm.features.ethereumSecurity'),
+      t('home.developer.zkevm.features.highScalability'),
+      t('home.developer.zkevm.features.lowFees'),
+      t('home.developer.zkevm.features.access'),
+      t('home.developer.zkevm.features.developerExperience'),
     ],
   },
-];
+]
 </script>
--- data/links.ts
+++ data/links.ts
@@ -1,62 +1,62 @@
-export const ambassadorProgramUrl =
-  "https://astarnetwork.notion.site/Astar-Space-Club-2023-5e396c75fcf34d698baf288d9aa9230f?pvs=4";
+export const ambassadorProgramUrl
+  = 'https://astarnetwork.notion.site/Astar-Space-Club-2023-5e396c75fcf34d698baf288d9aa9230f?pvs=4'
 
-export const bugBountyProgramUrl = "https://immunefi.com/bounty/astarnetwork";
+export const bugBountyProgramUrl = 'https://immunefi.com/bounty/astarnetwork'
 
-export const forumUrl = "https://forum.astar.network/";
+export const forumUrl = 'https://forum.astar.network/'
 
 export const brandAssetKitUrl = {
-  zip: "/astar-brand-asset-kit.zip",
+  zip: '/astar-brand-asset-kit.zip',
   figma:
-    "https://www.figma.com/file/xEu8dYHuvpraJhI7HfyG7y/Brand-Asset-Kit?type=design&node-id=0%3A1&mode=design&t=oMyNB877IZkMUUeB-1",
-};
+    'https://www.figma.com/file/xEu8dYHuvpraJhI7HfyG7y/Brand-Asset-Kit?type=design&node-id=0%3A1&mode=design&t=oMyNB877IZkMUUeB-1',
+}
 
 export const docsUrl = {
-  index: "https://docs.astar.network/",
-  dappStaking: "https://docs.astar.network/docs/learn/dapp-staking/",
-  networks: "https://docs.astar.network/docs/learn/networks/",
-};
+  index: 'https://docs.astar.network/',
+  dappStaking: 'https://docs.astar.network/docs/learn/dapp-staking/',
+  networks: 'https://docs.astar.network/docs/learn/networks/',
+}
 
 export const newsletterUrl = {
   global:
-    "https://cdn.forms-content.sg-form.com/429c438b-0fe0-11ee-8fa4-6a66fcc72380",
+    'https://cdn.forms-content.sg-form.com/429c438b-0fe0-11ee-8fa4-6a66fcc72380',
   japan:
-    "https://cdn.forms-content.sg-form.com/14380152-3085-11ee-8b23-becd142e98bb",
-};
+    'https://cdn.forms-content.sg-form.com/14380152-3085-11ee-8b23-becd142e98bb',
+}
 
 export const socialUrl = {
-  github: "https://github.com/AstarNetwork",
-  discord: "https://discord.gg/astarnetwork",
-  wellfound: "https://wellfound.com/company/astar-network",
-  youtube: "https://www.youtube.com/c/AstarNetwork",
-  telegram: "https://t.me/PlasmOfficial",
-  medium: "https://medium.com/astar-network",
+  github: 'https://github.com/AstarNetwork',
+  discord: 'https://discord.gg/astarnetwork',
+  wellfound: 'https://wellfound.com/company/astar-network',
+  youtube: 'https://www.youtube.com/c/AstarNetwork',
+  telegram: 'https://t.me/PlasmOfficial',
+  medium: 'https://medium.com/astar-network',
   twitter: {
     global: {
-      url: "https://twitter.com/AstarNetwork",
-      id: "@AstarNetwork",
+      url: 'https://twitter.com/AstarNetwork',
+      id: '@AstarNetwork',
     },
     japan: {
-      url: "https://twitter.com/AstarNetwork_JP",
-      id: "AstarNetwork_JP",
+      url: 'https://twitter.com/AstarNetwork_JP',
+      id: 'AstarNetwork_JP',
     },
     korea: {
-      url: "https://twitter.com/AstarNetwork_KR",
-      id: "AstarNetwork_KR",
+      url: 'https://twitter.com/AstarNetwork_KR',
+      id: 'AstarNetwork_KR',
     },
   },
-  reddit: "https://www.reddit.com/r/AstarNetwork/",
-};
+  reddit: 'https://www.reddit.com/r/AstarNetwork/',
+}
 
 export const grantsUrl = {
-  inkubator: "https://use.ink/ubator/",
-};
+  inkubator: 'https://use.ink/ubator/',
+}
 
 export const portalUrl = {
-  index: "https://portal.astar.network/",
-  dappStaking: "https://portal.astar.network/astar/dapp-staking/discover",
-};
+  index: 'https://portal.astar.network/',
+  dappStaking: 'https://portal.astar.network/astar/dapp-staking/discover',
+}
 
-export const careerUrl = "https://boards.greenhouse.io/startale";
+export const careerUrl = 'https://boards.greenhouse.io/startale'
 
-export const startaleUrl = "https://startale.org/";
+export const startaleUrl = 'https://startale.org/'
