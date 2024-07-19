4f84cde2ed97485d41fed679a61628721d66ffe9
Ayumi Takahashi <ayumee528@gmail.com>
2024-07-19 13:29:58 -0700

    Revert "clean up"
    This reverts commit c31f437d1ca340f27585f17a50ff56eed48ca372.

--- components/Header.vue
+++ components/Header.vue
@@ -176,7 +176,7 @@ const menus: Menu[] = [
             label: t('header.solution.startale.label'),
             description: t('header.solution.startale.description'),
             href: startaleUrl,
-            icon: '/images/common/startale-symbol.svg',
+            icon: '/images/header/startale-symbol.svg',
           },
           {
             label: t('header.solution.japan.label'),
